539da13e4538fc4072ae0c5158329c1f559b2fb2
betanyeli <betangelii@gmail.com>
2022-09-28 22:19:12 -0300

    feat: initial commit cachupinapp

--- apps/cachupinapp/src/app/App.spec.tsx
+++ apps/cachupinapp/src/app/App.spec.tsx
@@ -3,7 +3,15 @@ import { render } from '@testing-library/react-native';
 
 import App from './App';
 
-test('renders correctly', () => {
-  const { getByTestId } = render(<App />);
-  expect(getByTestId('heading')).toHaveTextContent('Welcome');
+describe('App.tsx', () => {
+
+  it('Should generate a snapshot', () => {
+    const { toJSON } = render(<App />);
+    expect(toJSON).toMatchSnapshot();
+  });
+  it('Should render correctly', () => {
+    const { getByText } = render(<App />);
+    expect(getByText('Cachupinapp')).toBeTruthy();
+  });
 });
+
